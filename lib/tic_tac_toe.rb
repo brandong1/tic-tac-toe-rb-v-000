7cd0bdfa0c3f9f9985bc2ed21c48cb75023037db
Brandon Green <brandon@brandonleeproductions.com>
2018-05-13 01:08:26 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -84,4 +84,56 @@
     end 
   end
   
+  def won?(board)
+    WIN_COMBINATIONS.each do |win_combination|
+      win_index_1 = win_combination[0]
+      win_index_2 = win_combination[1]
+      win_index_3 = win_combination[2]
+   
+      
+      position_1 = board[win_index_1]
+      position_2 = board[win_index_2]
+      position_3 = board[win_index_3]
   
+      if position_1 == "X" && position_2 == "X" && position_3 == "X" || position_1 == "O" && position_2 == "O" && position_3 == "O"
+        return win_combination
+  
+      end 
+    end
+    return false 
+  end
+  
+  
+  def full?(board)
+    board.all? do |index|
+      index == "X" || index == "O"
+     end  
+    
+  end
+  
+  
+  def draw?(board)
+    if won?(board) == false && full?(board) == true 
+      return true
+    end 
+    false
+  end
+  
+  
+  def over?(board)
+    won?(board) || full?(board) 
+  end
+    
+  
+  def winner(board)
+    if won?(board) == false
+      nil
+    else
+      x = won?(board)[0]
+      if board[x] == "X"
+        "X"
+      else
+        "O"
+      end
+    end
+  end
