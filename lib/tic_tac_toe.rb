e0364c00f4490e81540a2f647c87146dbec1f820
Brandon Green <brandon@brandonleeproductions.com>
2018-05-13 01:06:33 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -62,4 +62,24 @@
     
   end
   
+  def turn_count(board)
+    counter = 0
+    board.each do |turn|
+      if turn == "X" || turn == "O"
+        counter += 1 
+      end 
+    end
+    counter
+  end
   
+  def current_player(board)
+    turn = "X"
+    if turn_count(board) % 2 == 0 
+      puts "X" 
+       turn
+    else
+      turn = "O"
+      puts "O"
+      turn
+    end 
+  end
