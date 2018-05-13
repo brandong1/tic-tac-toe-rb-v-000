4037e33b872bb17663757e7e019be1b04aff7ec5
Brandon Green <brandon@brandonleeproductions.com>
2018-05-13 01:04:38 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -10,3 +10,56 @@
     [6, 4, 2]
     
   ]
+  
+  def display_board(board)
+    puts " #{board[0]} | #{board[1]} | #{board[2]} "
+    puts "-----------"
+    puts " #{board[3]} | #{board[4]} | #{board[5]} "
+    puts "-----------"
+    puts " #{board[6]} | #{board[7]} | #{board[8]} "
+  end
+  
+  def turn(board)
+    puts "Please enter 1-9:"
+  
+    input = gets.strip # I'm getting a users input 
+    index = input_to_index(input) # Calling input_to_index method
+  
+    if valid_move?(board, index) 
+      puts "Valid Move"
+      move(board, index) # Calls the move method
+      display_board(board) # Displays board with updated index
+    else
+     puts "Invalid move. Try again."
+     turn(board) # Re-runs the turn() method (i.e. re-runs this code block)
+    end
+  end
+  
+  
+  def input_to_index(move)
+     index = move.to_i - 1 # Declares the index variable, changes it to an integer and -1 to correspond to the array indexes
+     index
+  end
+  
+  def valid_move?(board, index)
+    if index.to_i.between?(0, 8) && !position_taken?(board, index)
+      true 
+    else
+      false 
+    end
+  end
+  
+  def position_taken?(board, index)
+      if board[index] == " " || board[index] == "" || board[index] == nil
+          false
+      else
+          true
+      end
+  end
+  
+  def move(board, index, player = "X") # In this instance, we give player a default value of "X"
+    board[index] = player # Updates the board array index location with player, or in this case, the default value "X"
+    
+  end
+  
+  
